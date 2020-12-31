2b4b07ad3459dc4b2117c96460bfbb586311637a
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-12-31 14:32:44 -0500

    21 Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,27 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://www.plymouth.edu/theclock/wp-content/uploads/sites/183/2019/03/stan-lee.png'),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://www.plymouth.edu/theclock/wp-content/uploads/sites/183/2019/03/stan-lee.png'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
