195c9e0af40651be9389fb0a1dca917f0f1e06d2
Victor Bejarano <alejandrobjrn4@gmail.com>
2020-12-31 09:51:01 -0500

    11 Future Builder

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -15,17 +15,35 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    menuProvider.cargarData();
-    return ListView(
-      children: _listaItems(),
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      // initialData: [],
+      builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+        print('builder');
+        print(snapshot.data);
+        return ListView(
+          children: _listaItems(snapshot.data),
+        );
+      },
     );
   }
 
-  List<Widget> _listaItems() {
-    return [
-      ListTile(
-        title: Text('Hola'),
-      )
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(
+          Icons.account_circle,
+          color: Colors.blue,
+        ),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {},
+      );
+      opciones..add(widgetTemp)..add(Divider());
+    });
+
+    return opciones;
   }
 }
